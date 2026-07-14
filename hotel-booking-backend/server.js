dbffa1e400c2941c0a2a451e25113810fb98d321
Ragul Suresh <ragul@sohutechnologies.com>
2026-07-14 20:17:52 +0530

    backend live url added in cors

--- hotel-booking-backend/server.js
+++ hotel-booking-backend/server.js
@@ -78,8 +78,10 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
+
+  "https://hotel-management-system-frontend-seven.vercel.app",
+
   "https://hotel-management-system-backend-five.vercel.app"
-  
 ];
 
 app.use(cors({
